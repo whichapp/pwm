7104e1f405ceb31956afa378447bd69a0c7d763b
Bartek <lelevel@gmail.com>
2017-02-24 09:25:19 +0000

    Creating a View Class

--- js/main.js
+++ js/main.js
@@ -1,12 +1,5 @@
 var app = {
 
-	findByName: function() {
-		var self = this;
-		this.store.findByName($('.search-key').val(), function(employees) {
-			$('.employee-list').html(self.employeeLiTpl(employees));
-		});
-	},
-
 	showAlert: function (message, title) {
 		if (navigator.notification) {
 			navigator.notification.alert(message, null, title, 'OK');
@@ -15,22 +8,12 @@ var app = {
 		}
 	},
 
-	
-	renderHomeView: function() {
-		$('body').html(this.homeTpl());
-		$('.search-key').on('keyup', $.proxy(this.findByName, this));
-	},
-	
-
 	initialize: function() {
 		var self = this;
 		this.store = new MemoryStore(function() {
-			self.renderHomeView();
+			$('body').html(new HomeView(self.store).render().el);
 		});
-		this.homeTpl = Handlebars.compile($("#home-tpl").html());
-		this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
-	}
-	
+	}	
 	
 	
 };
